3e417ae4f859d51877152799ad63744450de9816
Bùi Ánh Nguyệt <144470995+Nguyett11@users.noreply.github.com>
2025-05-25 21:34:13 +0700

    Update student.report-list.component.ts

--- src/app/components/student/student.report-list/student.report-list.component.ts
+++ src/app/components/student/student.report-list/student.report-list.component.ts
@@ -24,9 +24,6 @@ interface ReportItem {
 })
 
 export class StudentReportListComponent {
-  //reportList: report[];
-  //reports : report[];
-
   reportList: ReportItem[] = [];
   reports: ReportItem[] = [];
   errorMessage : string;
@@ -79,66 +76,6 @@ export class StudentReportListComponent {
         console.error('Error fetching students:', error);
       }
     });
+  }
 }
-    
-
-    // getAllReports() {
-    //   const userId = Number(localStorage.getItem('user_id'));
-    //   this.studentService.getStudentById(userId).subscribe({
-    //     next: (data) => {
-    //       this.reportStudentService.getReportsByStudentCode(data.student_code).subscribe({
-    //         next: (response: any) => {
-    //           this.reportList = response;
-    
-    //           const reportRequests = this.reportList.map(report =>
-    //             this.reportService.getReportById(report.Report.report_id)
-    //           );
-    
-    //           forkJoin(reportRequests).subscribe({
-    //             next: (reportsData : report[]) => {
-    //               this.reports = reportsData;
-    //               console.log(this.reports); 
-    //             },
-    //             error: (err) => {
-    //               console.error('Lỗi khi lấy chi tiết các báo cáo:', err);
-    //             }
-    //           });
-    //         },
-    //         error: (error: any) => {
-    //           console.error('Error fetching reports by student code:', error);
-    //         }
-    //       });
-    //     },
-    //     error: (error: any) => {
-    //       console.error('Error fetching student by ID:', error);
-    //     }
-    //   });
-    // }
-    
-}
-
-
-// this.reportService.getAllReports().subscribe({
-//   next: (data) => {
-//     this.reportList = data;
-//     console.log("kk", data);
-
-    // Duyệt qua từng report và lấy thông tin submission theo id_report
-    // this.reportList.forEach((report: any) => {
-    //   console.log("hh: ", report.id_report);
-    //   this.submissionService.getSubmissionByIdReport(report.id_report).subscribe({
-    //     next: (submissionData) => {
-    //       // Gắn thông tin submission vào report tương ứng
-    //       report.submission = submissionData;
-    //       console.log(`Submission cho report ${report.id_report}:`, submissionData);
-    //     },
-    //     error: (err) => {
-    //       console.error(`Lỗi lấy submission cho report ${report.id_report}:`, err);
-    //     }
-    //   });
-    // });
-//   },
-//   error: (err) => {
-//     console.error('Lỗi lấy danh sách báo cáo:', err);
-//   }
-// });
+  
