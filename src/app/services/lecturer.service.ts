ac9f37896d35180140256ac5a43f3c28b82c9cdc
Bùi Ánh Nguyệt <144470995+Nguyett11@users.noreply.github.com>
2025-05-25 21:38:21 +0700

    Update lecturer.service.ts

--- src/app/services/lecturer.service.ts
+++ src/app/services/lecturer.service.ts
@@ -15,7 +15,7 @@ export class LecturerService {
   constructor(private http: HttpClient) { }
 
   getAllLecturers(): Observable<Lecturer[]> {
-    const token = localStorage.getItem('token'); // Lấy token JWT từ localStorage hoặc sessionStorage
+    const token = localStorage.getItem('token'); 
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
@@ -23,7 +23,7 @@ export class LecturerService {
   }
 
   getLecturerById(id: number): Observable<Lecturer> {
-    const token = localStorage.getItem('token'); // Lấy token JWT từ localStorage hoặc sessionStorage
+    const token = localStorage.getItem('token'); 
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
@@ -32,7 +32,7 @@ export class LecturerService {
   }
 
   searchLecturersByName(name: string): Observable<any[]> {
-    const token = localStorage.getItem('token'); // Lấy JWT
+    const token = localStorage.getItem('token'); 
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
@@ -45,49 +45,19 @@ export class LecturerService {
 
   getLecturerByUserId(userId: number): Observable<any> {
     const headers = new HttpHeaders({
-      'Authorization': `Bearer ${localStorage.getItem('token')}` // Giả sử bạn lưu token trong localStorage
+      'Authorization': `Bearer ${localStorage.getItem('token')}` 
     });
 
     return this.http.get(`${this.api}/ByUserId/${userId}`, { headers });
   }
 
   updateLecturer(id: number, body: any): Observable<any> {
-    const token = localStorage.getItem('token'); // Lấy token JWT từ localStorage hoặc sessionStorage
+    const token = localStorage.getItem('token'); 
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
 
     const url = `${this.api}/${id}`;
     return this.http.put<any>(url, body, { headers });
-  }
-
-  getLectures( keyword: string, page: number, limit: number
-  ): Observable<Lecturer[]> {
-    const params = {
-      keyword: keyword,
-      page: page.toString(),
-      limit: limit.toString()
-    };
-    return this.http.get<Lecturer[]>(this.api, { params });
-  }
-
-  getDetailLecturer(id: number): Observable<Lecturer> {
-    return this.http.get<Lecturer>(`${this.api}/${id}`);
-  }
-
-  insertLecturer(body: any): Observable<any> {
-    return this.http.post(`http://localhost:8080/api/lecturers`, body).pipe(
-      tap(response => console.log("Response from server:", response)) // Log phản hồi từ server
-    );
-  }
-  // updateLecturer(id: number, body: any): Observable<any> {
-  //   const url = `${this.api}/${id}`;
-  //   return this.http.put<any>(url, body);
-  // }
-  // Thêm phương thức lấy thông tin lecturer bằng token
-  getLecturerByToken(): Observable<any> {
-    return this.http.get<any>(`${this.api}/by-token`).pipe(
-      tap(response => console.log("Lecturer info from token:", response)) // Log phản hồi từ server
-    );
   }
 }
